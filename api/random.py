9b39aec4abce201280091cf18816bef08fc1b739
Sadman Ishtiak <sadmanishtiak1@gmail.com>
2026-04-30 22:36:10 +0600

    a

--- api/random.py
+++ api/random.py
@@ -1,14 +1,18 @@
+from http.server import BaseHTTPRequestHandler
 import random
+import json
 
-def handler(request):
-    number = random.randint(1, 100)
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": {
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        number = random.randint(1, 100)
+        
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        
+        response = {
             "random_number": number
         }
-    }
+        
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+        return
